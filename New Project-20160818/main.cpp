6807f1d1141900b122e8ef98b66123386f9a728b
Walter E. Gunter, Jr <wegunterjr@users.noreply.github.com>
2016-08-17 20:33:24 -0600

    Created by Coding Ground Client

---
+++ New Project-20160818/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "rectangle.h"
+#include "triangle.h"
+#include "VirtualFirst.h"
+using namespace std;
+
+void ChargeContactor::Contactor()
+{
+    cout << " inside the ChargeContactor::Contactor(): " << endl;
+}
+
+int main()
+{
+   cout << "Hello World" << endl; 
+  
+/*   Rectangle rect;
+  Triangle trgl;
+  Polygon * ppoly1 = &rect;
+  Polygon * ppoly2 = &trgl;
+  ppoly1->set_values (4,5);
+  ppoly2->set_values (4,5);
+  cout << rect.area() << '\n';
+  cout << trgl.area() << '\n';
+  */
+  
+   return 0;
+}
+
+
